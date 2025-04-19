738c18d37465e9c0afa26e044119aa5af749e6fd
Chandan Pandey <chandanpandey1928@gmail.com>
2025-04-19 13:45:12 +0530

    Initial commit

---
+++ src/class_38_Launch/Selenium/class_38_LaunchBrowser.java
@@ -0,0 +1,9 @@
+package class_38_Launch.Selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class class_38_LaunchBrowser {
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();
+	}
+}
