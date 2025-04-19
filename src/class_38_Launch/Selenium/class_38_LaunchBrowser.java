21f72da93dd39a13da7b6a9f464d6a1325add36f
Chandan Pandey <chandanpandey1928@gmail.com>
2025-04-19 14:01:03 +0530

    launch browser and get methods

--- src/class_38_Launch/Selenium/class_38_LaunchBrowser.java
+++ src/class_38_Launch/Selenium/class_38_LaunchBrowser.java
@@ -4,6 +4,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class class_38_LaunchBrowser {
 	public static void main(String[] args) {
+
 		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in");// get method can help you to take you to any particular Websites
+		String title = driver.getTitle();// getTitle method can help to get the title of the page
+		System.out.println(title);
+		driver.quit();
+		//driver.close();
 	}
 }
