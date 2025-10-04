92ab2ddb575b3cdeb5e663b7d6c7f1afebd7f78d
Chandan Pandey <chandanpandey1928@gmail.com>
2025-10-04 10:43:01 +0530

    changes

--- src/class_50_TakeScreenShot_Math_Random/Amazon_TakeScreenShot_Math_Random.java
+++ src/class_50_TakeScreenShot_Math_Random/Amazon_TakeScreenShot_Math_Random.java
@@ -8,24 +8,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.io.FileHandler;
 
 public class Amazon_TakeScreenShot_Math_Random {
-	
+	static WebDriver driver;
+
 	public static void main(String[] args) throws IOException {
-		ChromeDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.navigate().to("https://www.amazon.in");
 		WebElement search = driver.findElement(By.id("twotabsearchtextbox"));
 		search.sendKeys("I phone 16 pro max");
 		search.sendKeys(Keys.ENTER);
-		TakesScreenshot tss = driver;
+		TakesScreenshot tss = (TakesScreenshot) driver;
 		File source = tss.getScreenshotAs(OutputType.FILE);
-		//File destination = new File("C:\\Users\\mukul\\eclipse-workspace\\Chandan_Selenium_Project\\Screenshots\\Two"+Math.random() +".jpeg");
-		File destination = new File("C:\\Users\\mukul\\eclipse-workspace\\Chandan_Selenium_Project\\Screenshots\\Two"+new Amazon_TakeScreenShot_Math_Random().getClass() +".jpeg");
+		// File destination = new
+		// File("C:\\Users\\mukul\\eclipse-workspace\\Chandan_Selenium_Project\\Screenshots\\Two"+Math.random()
+		// +".jpeg");
+		File destination = new File("C:\\Users\\mukul\\eclipse-workspace\\Chandan_Selenium_Project\\Screenshots\\Two"
+				+ new Amazon_TakeScreenShot_Math_Random().getClass() + ".jpeg");
 		FileHandler.copy(source, destination);
 	}
 
